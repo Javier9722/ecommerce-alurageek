0f0da0c5448f65fd0c270c778e8cc2fb7e72b73b
Javier9722 <eduardo.javier.rf@gmail.com>
2023-04-21 09:05:29 -0500

    delete lottie react and imgBanner, add cdn lottie

--- src/components/bannerItems/ImgBanner.jsx
+++ src/components/bannerItems/ImgBanner.jsx
@@ -1,15 +1,18 @@
 import React from "react";
-import Lottie from "lottie-react";
-import garden from "./garden.json";
 
 const App = () => {
   return (
     // <div className="w-[200px] h-[200px] sm:w-[300px] sm:h-[300px] rounded-xl overflow-hidden border border-red-300"></div>
-    <Lottie
-      animationData={garden}
-      loop={true}
+    // <Lottie
+    //   animationData={garden}
+    //   loop={true}
+    //   className="shrink-0 w-[200px] h-[200px] sm:w-[300px] sm:h-[300px] rounded-xl overflow-hidden border border-red-300"
+    // />
+    <lottie-player
+      id="firstLottie"
+      src="https://assets2.lottiefiles.com/packages/lf20_o6hQ8m.json"
       className="shrink-0 w-[200px] h-[200px] sm:w-[300px] sm:h-[300px] rounded-xl overflow-hidden border border-red-300"
-    />
+    ></lottie-player>
   );
 };
 
